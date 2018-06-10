4899a4fdaf1796553856027cc32a5ffec17461de
Zhenyuan <lizhenyuan@zju.edu.cn>
2018-06-11 04:49:34 +0800

    init commit

---
+++ TraceEvent2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Diagnostics.Tracing.Parsers;
+
+namespace TraceEvent2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TraceEventSamples.AllSamples.Run();
+
+            //testFunc();
+            return;
+        }
+
+        static void testFunc()
+        {
+            Console.Write(RegisteredTraceEventParser.GetManifestForRegisteredProvider("Microsoft-Windows-NDIS-PacketCapture"));
+        }
+    }
+}
