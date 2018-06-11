827ad50139978eb4d6c126070270e0b7912774c5
Zhenyuan <lizhenyuan@zju.edu.cn>
2018-06-12 07:07:58 +0800

    parse logfile

--- TraceEvent2/Program.cs
+++ TraceEvent2/Program.cs
@@ -5,6 +5,12 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Diagnostics;
+
+using Gnu.Getopt;
+using NDesk.Options;
+
+using Microsoft.Diagnostics;
 using Microsoft.Diagnostics.Tracing;
 using Microsoft.Diagnostics.Tracing.Parsers;
 using Microsoft.Diagnostics.Tracing.Session;
@@ -14,153 +20,104 @@ namespace TraceEvent2
     class Program
     {
         public static TextWriter Out = Console.Out;
+        public static TextWriter logOut = Console.Out;
+        public static TextWriter dataOut = Console.Out;
+
+        static List<string> logFileList = new List<string>();
+        static List<string> providerNameList = new List<string>();
 
         static void Main(string[] args)
         {
-            //TraceEventSamples.AllSamples.Run();
+            // Parse CommandLine Arguments
+            bool show_help = false;
+            var commandLineParser = new OptionSet()
+            {
+                {"l|logfile=", "The logfile's path.", logFile => logFileList.Add(logFile)},
+                {"p|provider=", "The provider's names.", providerName => providerNameList.Add(providerName)},
+                {"o|output=", "The output file path.", outputPath => setDataOut(outputPath)},
+                {"h|help", "Show help information.", v => show_help = v != null}
+            };
 
-            //DynamicParserTest();
-            RegisterParserTest();
+            try
+            {
+                commandLineParser.Parse(args);
+            }
+            catch
+            {
+                logOut.WriteLine("Parse command line failed");
+                ShowHelp(null);
+                return;
+            }
+            if (show_help) ShowHelp(commandLineParser);
 
+            // Start Parse Events, if there exit
+            if(logFileList.Count() != 0)
+            {
+                ParseLogFile();
+            }
+
+#if DEBUG
+            Debugger.Break();
+#endif
             return;
         }
 
-        static void RegisterParserTestOnline()
+        private static void ShowHelp(OptionSet p)
         {
-            var monitoringTimeSec = 1000;
-
-            if (Environment.OSVersion.Version.Major * 10 + Environment.OSVersion.Version.Minor < 62)
-            {
-                Out.WriteLine("This demo only works on Win8 / Win 2012 an above)");
-                return;
-            }
-
-            Out.WriteLine("******************** KernelAndClrMonitor DEMO (Win 8) ********************");
-            Out.WriteLine("Printing both Kernel and CLR (user mode) events simultaneously");
-            Out.WriteLine("The monitor will run for a maximum of {0} seconds", monitoringTimeSec);
-            Out.WriteLine("Press Ctrl-C to stop monitoring early.");
-            Out.WriteLine();
-            Out.WriteLine("Start a .NET program to see some events!");
-            Out.WriteLine();
-            if (TraceEventSession.IsElevated() != true)
-            {
-                Out.WriteLine("Must be elevated (Admin) to run this program.");
-                return;
-            }
-
-            TraceEventSession session = null;
-
-            // Set up Ctrl-C to stop both user mode and kernel mode sessions
-            Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs cancelArgs) =>
-            {
-                if (session != null)
-                    session.Dispose();
-                cancelArgs.Cancel = true;
-            };
-
-            // Set up a timer to stop processing after monitoringTimeSec 
-            var timer = new Timer(delegate (object state)
-            {
-                Out.WriteLine("Stopped Monitoring after {0} sec", monitoringTimeSec);
-                if (session != null)
-                    session.Dispose();
-            }, null, monitoringTimeSec * 1000, Timeout.Infinite);
-
-            // Create the new session to receive the events.  
-            // Because we are on Win 8 this single session can handle both kernel and non-kernel providers.  
-            using (session = new TraceEventSession("MonitorKernelAndClrEventsSession"))
-            {
-                // Enable the events we care about for the kernel
-                // For this instant the session will buffer any incoming events.  
-                // Enabling kernel events must be done before anything else.   
-                // This will fail on Win7.  
-                Out.WriteLine("Enabling Image load, Process and Thread events.");
-                session.EnableKernelProvider(
-                    KernelTraceEventParser.Keywords.ImageLoad |
-                    KernelTraceEventParser.Keywords.Process |
-                    KernelTraceEventParser.Keywords.Thread);
-
-                // Subscribe the events of interest.   In this case we just print all events.  
-                session.Source.Kernel.All += Print;
-
-                Out.WriteLine("Enabling CLR GC and Exception events.");
-                // Enable the events we care about for the CLR (in the user session).
-                // unlike the kernel session, you can call EnableProvider on other things too.  
-                // For this instant the ;session will buffer any incoming events.  
-                //session.EnableProvider(
-                //    ClrTraceEventParser.ProviderGuid,
-                //    TraceEventLevel.Informational,
-                //    (ulong)(ClrTraceEventParser.Keywords.GC | ClrTraceEventParser.Keywords.Exception));
-
-                session.Source.Clr.All += Print;
-#if DEBUG
-                // in debug builds it is useful to see any unhandled events because they could be bugs. 
-                session.Source.UnhandledEvents += Print;
-#endif
-                // process events until Ctrl-C is pressed or timeout expires
-                Out.WriteLine("Waiting for Events.");
-                session.Source.Process();
-            }
-
-            timer.Dispose();    // Turn off the timer.  
+            Out.WriteLine("Usage:");
+            p.WriteOptionDescriptions(Out);
         }
 
-        static void Print(TraceEvent data)
+        private static void setDataOut(string outputPath)
+        {
+            try
+            {
+                FileStream fs =  new FileStream(outputPath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                dataOut = new StreamWriter(fs);
+            }
+            catch
+            {
+                logOut.WriteLine("Can't open " + outputPath);
+            }
+        }
+
+        public static void ParseLogFile()
+        {
+            foreach (var logfile in logFileList)
+            {
+                try { 
+                    using (var source = new ETWTraceEventSource(logfile))
+                    {
+                        if (source.EventsLost != 0)
+                            Out.WriteLine("WARNING: there were {0} lost events", source.EventsLost);
+
+                        // Set up callbacks to 
+                        source.Clr.All += Print;
+                        source.Kernel.All += Print;
+
+                        var symbolParser = new RegisteredTraceEventParser(source);
+                        symbolParser.All += Print;
+
+                        source.Process();
+                        Out.WriteLine("Done Processing.");
+                    }
+                }
+                catch
+                {
+                    logOut.WriteLine(logfile + " not found!");
+                }
+            }
+        }
+
+            static void Print(TraceEvent data)
         {
             // There are a lot of data collection start on entry that I don't want to see (but often they are quite handy
             if (data.Opcode == TraceEventOpcode.DataCollectionStart)
                 return;
 
-            Out.WriteLine(data.ToString());
+            dataOut.WriteLine(data.ToString());
             if (data is UnhandledTraceEvent)
-                Out.WriteLine(data.Dump());
-        }
-
-        static void DynamicParserTest()
-        {
-            var monitoringTimeSec = 500;
-
-            if (!(TraceEventSession.IsElevated() ?? false))
-            {
-                Out.WriteLine("To turn on ETW events you need to be Administrator, please run from an Admin process.");
-            }
-
-            var sessionName = "SimpleMontitorSession";
-
-            using (var session = new TraceEventSession(sessionName))
-            { 
-                Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs e) { session.Dispose(); };
-
-                var firstEventTimeMSec = new Dictionary<int, double>();
-
-                session.Source.Dynamic.All += delegate (TraceEvent data)
-                {  
-                    var delay = (DateTime.Now - data.TimeStamp).TotalSeconds;
-                    Out.WriteLine("GOT Event Delay={0:f1}sec: {1} ", delay, data.ToString());
-                };
-
-
-                var restarted = session.EnableProvider("Microsoft-Windows-Kernel-File");
-                if (restarted)      // Generally you don't bother with this warning, but for the demo we do. 
-                    Out.WriteLine("The session {0} was already active, it has been restarted.", sessionName);
-
-                // Set up a timer to stop processing after monitoringTimeSec
-                var timer = new Timer(delegate (object state)
-                {
-                    Out.WriteLine("Stopped after {0} sec", monitoringTimeSec);
-                    session.Source.StopProcessing();
-                }, null, monitoringTimeSec * 1000, Timeout.Infinite);
-
-
-                Out.WriteLine("**** Start listening for events from the Microsoft-Windows-Kernel-File provider.");
- 
-                session.Source.Process();
-
-                timer.Dispose();    // Done with the timer.  
-                Out.WriteLine();
-                Out.WriteLine("Stopping the collection of events.");
-            }
-
+                dataOut.WriteLine(data.Dump());
         }
     }
 }
---
+++ TraceEvent2/Readme.md
@@ -0,0 +1,5 @@
+Usage:
+  -l, --logfile=VALUE        The logfile's path.
+  -p, --provider=VALUE       The provider's names.
+  -o, --output=VALUE         The output file path.
+  -h, --help                 Show help information.
