d79badf9060ed10a8ce2e14e69016cd0c2121053
zhenyuan <lizhenyuan@zju.edu.cn>
2018-11-19 11:16:23 +0800

    usage of AstVisit

---
+++ PowershellInteract/PowershellInstance.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Management.Automation;
+using System.Management.Automation.Language;
+using System.Collections.ObjectModel;
+
+namespace TraceEvent2.PowerShellInteract
+{
+    class PowershellInstance
+    {
+        PowerShell psInstance = PowerShell.Create();
+
+        public PowershellInstance()
+        {
+
+        }
+
+        public void TestDetection()
+        {
+            PowerShell PowershellInstance = PowerShell.Create();
+            PowershellInstance.AddScript("param($param1) $d = get-date; $s = 'test string value'; " +
+                "$d; $s; $param1; get-service");
+            PowershellInstance.AddParameter("param1", "parameter 1 value!");
+            Collection<PSObject> PSOutput = PowershellInstance.Invoke();
+            foreach (PSObject outputItem in PSOutput)
+            {
+
+            }
+#if DEBUG
+            System.Diagnostics.Debugger.Break();
+#endif
+        }
+
+    }
+}
